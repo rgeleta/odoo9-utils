9a7ee6661152675be36c9738c5764ac814eb0fd0
Robert Geleta <rgeleta@users.noreply.github.com>
2015-11-08 16:13:43 -0800

    another reference for fixup script

--- scripts/odoo9-install/odoo9-fixup.sh
+++ scripts/odoo9-install/odoo9-fixup.sh
@@ -5,6 +5,7 @@ sudo apt-get install python-passlib
 #
 # image problems - Ref:
 # http://blog.instant-erp.com/2015/10/install-odoo-v90-with-nodejs-on-ubuntu.html
+# http://stackoverflow.com/questions/8915296/python-image-library-fails-with-message-decoder-jpeg-not-available-pil
 # 
 # install jpeg library
 sudo apt-get install libjpeg-dev
